4cb71880c75a9115bb43f72e9e08ca8766aade32
Dmitry Kushnir <dmitry.kushnir@outlook.com>
2025-02-19 13:46:06 +0000

    added readme

--- AngleSharp.ReadOnlyDom.Benchmarks/ParserBenchmark.cs
+++ AngleSharp.ReadOnlyDom.Benchmarks/ParserBenchmark.cs
@@ -1,11 +1,9 @@
 using System;
 using System.Collections.Generic;
-using AngleSharp.Dom;
 using AngleSharp.Html.Parser;
 using AngleSharp.Html.Parser.Tokens.Struct;
 using AngleSharp.ReadOnlyDom.Filters;
 using AngleSharp.ReadOnlyDom.Helpers;
-using AngleSharp.Text;
 using BenchmarkDotNet.Attributes;
 using BenchmarkDotNet.Configs;
 using BenchmarkDotNet.Engines;
@@ -72,53 +70,16 @@ namespace AngleSharp.ReadOnlyDom.Benchmarks
                 new[]{
                 "http://www.nytimes.com",
                 "http://www.amazon.com",
-                // "http://www.blogspot.com",
-                // "http://www.smashing.com",
-                // "http://www.youtube.com",
-                // "http://www.weibo.com",
-                // "http://www.yahoo.com",
-                // "http://www.google.com",
-                // "http://www.linkedin.com",
                 "http://www.pinterest.com",
-                // "http://news.google.com",
-                // "http://www.baidu.com",
-                // "http://www.codeproject.com",
-                // "http://www.ebay.com",
                 "http://www.msn.com",
-                // "http://www.nbc.com",
-                // "http://www.qq.com",
                 "http://www.florian-rappl.de",
-                // "http://www.stackoverflow.com",
-                // "http://www.html5rocks.com/en",
-                // "http://www.live.com",
-                // "http://www.taobao.com",
-                // "http://www.huffingtonpost.com",
                 "http://www.wordpress.org",
-                // "http://www.myspace.com",
-                // "http://www.flickr.com",
-                // "http://www.godaddy.com",
-                // "http://www.reddit.com",
                 "http://www.nytimes.com",
-                // "http://peacekeeper.futuremark.com",
-                // "http://www.pcmag.com",
-                // "http://www.sitepoint.com",
                 "http://html5test.com",
                 "http://www.spiegel.de",
-                // "http://www.tmall.com",
-                // "http://www.sohu.com",
-                // "http://www.vk.com",
                 "http://www.wordpress.com",
-                // "http://www.bing.com",
-                // "http://www.tumblr.com",
-                // "http://www.ask.com",
                 "http://www.mail.ru",
                 "http://www.imdb.com",
-                // "http://www.kickass.to",
-                // "http://www.360.cn",
-                // "http://www.163.com",
-                // "http://www.neobux.com",
-                // "http://www.aliexpress.com",
-                // "http://www.netflix.com",
                 "http://www.w3.org/TR/html5/single-page.html",
                 "http://en.wikipedia.org/wiki/South_African_labour_law"
                 }).GetAwaiter().GetResult();
@@ -131,29 +92,13 @@ namespace AngleSharp.ReadOnlyDom.Benchmarks
         private readonly IBrowsingContext _context = ReadOnlyParser.DefaultContext;
 
         [Benchmark]
-        public bool BodyRO()
+        public bool JustHtmlBodyReadOnlyDOM()
         {
             var filter = new FirstTagAndAllChildren("body");
             var parser = new HtmlParser(HtmlParserOptions, _context);
             using var document = parser.ParseReadOnlyDocument(UrlTest.Source, filter.Loop);
             return false;
         }
-
-        // [Benchmark]
-        // public Boolean Readonly()
-        // {
-        //     var parser = new HtmlParser(DefaultOptions, _context);
-        //     using var document = parser.ParseReadOnlyDocument(UrlTest.Source);
-        //     return false;
-        // }
-        
-        // [Benchmark]
-        // public Boolean DefaultSTS()
-        // {
-        //     var parser = new HtmlParser(DefaultOptions, _context);
-        //     using var _ = parser.ParseDocument<Document, Element>(new TextSource(new StringTextSource(UrlTest.Source)));
-        //     return false;
-        // }
         
         [Benchmark(Baseline = true)]
         public bool Default()
