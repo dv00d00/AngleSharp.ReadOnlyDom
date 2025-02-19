7e912a93d07412dbecbc6344a78705217dd8fe49
Dmitry Kushnir <dmitry.kushnir@outlook.com>
2025-02-19 16:30:22 +0000

    medium run

--- AngleSharp.ReadOnlyDom.Benchmarks/ParserBenchmark.cs
+++ AngleSharp.ReadOnlyDom.Benchmarks/ParserBenchmark.cs
@@ -20,7 +20,7 @@ namespace AngleSharp.ReadOnlyDom.Benchmarks
         {
             public Config()
             {
-                AddJob(Job.ShortRun
+                AddJob(Job.MediumRun
                     .WithRuntime(CoreRuntime.Core80)
                     .WithStrategy(RunStrategy.Throughput)
                 );
@@ -70,16 +70,53 @@ namespace AngleSharp.ReadOnlyDom.Benchmarks
                 new[]{
                 "http://www.nytimes.com",
                 "http://www.amazon.com",
+                "http://www.blogspot.com",
+                "http://www.smashing.com",
+                "http://www.youtube.com",
+                "http://www.weibo.com",
+                "http://www.yahoo.com",
+                "http://www.google.com",
+                "http://www.linkedin.com",
                 "http://www.pinterest.com",
+                "http://news.google.com",
+                "http://www.baidu.com",
+                "http://www.codeproject.com",
+                "http://www.ebay.com",
                 "http://www.msn.com",
+                "http://www.nbc.com",
+                "http://www.qq.com",
                 "http://www.florian-rappl.de",
+                "http://www.stackoverflow.com",
+                "http://www.html5rocks.com/en",
+                "http://www.live.com",
+                "http://www.taobao.com",
+                "http://www.huffingtonpost.com",
                 "http://www.wordpress.org",
+                "http://www.myspace.com",
+                "http://www.flickr.com",
+                "http://www.godaddy.com",
+                "http://www.reddit.com",
                 "http://www.nytimes.com",
+                "http://peacekeeper.futuremark.com",
+                "http://www.pcmag.com",
+                "http://www.sitepoint.com",
                 "http://html5test.com",
                 "http://www.spiegel.de",
+                "http://www.tmall.com",
+                "http://www.sohu.com",
+                "http://www.vk.com",
                 "http://www.wordpress.com",
+                "http://www.bing.com",
+                "http://www.tumblr.com",
+                "http://www.ask.com",
                 "http://www.mail.ru",
                 "http://www.imdb.com",
+                "http://www.kickass.to",
+                "http://www.360.cn",
+                "http://www.163.com",
+                "http://www.neobux.com",
+                "http://www.aliexpress.com",
+                "http://www.netflix.com",
                 "http://www.w3.org/TR/html5/single-page.html",
                 "http://en.wikipedia.org/wiki/South_African_labour_law"
                 }).GetAwaiter().GetResult();
