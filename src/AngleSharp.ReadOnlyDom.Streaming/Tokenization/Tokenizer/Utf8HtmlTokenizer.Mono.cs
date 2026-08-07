844769711964402148ab2a0b6163f08828f008f8
Dmytro Kushnir <dkushnir@meddbase.com>
2026-08-07 14:45:43 +0200

    start cleanup

---
+++ src/AngleSharp.ReadOnlyDom.Streaming/Tokenization/Tokenizer/Utf8HtmlTokenizer.Mono.cs
@@ -0,0 +1,37 @@
+#pragma warning disable CS1591 // Experimental API surface; shape is intentionally unsettled.
+
+using System.IO.Pipelines;
+
+namespace AngleSharp.ReadOnlyDom.Streaming.Tokenization;
+
+internal sealed class Utf8HtmlTokenizer : Utf8HtmlTokenizer<EnforcedResourceLimits>
+{
+    public static ValueTask<Utf8HtmlTokenizerCounters> TokenizeAsync(
+        PipeReader reader,
+        IUtf8HtmlTokenSink sink,
+        CancellationToken cancellationToken = default,
+        HtmlStreamingLimits? limits = null,
+        Utf8InputContract inputContract = Utf8InputContract.ArbitraryBytes
+    )
+    {
+        limits ??= HtmlStreamingLimits.Default;
+        return Utf8HtmlTokenizerPipeline.TokenizeAsync(reader, sink, cancellationToken, limits, inputContract);
+    }
+
+    public Utf8HtmlTokenizer(IUtf8HtmlTokenSink sink)
+        : base(sink) { }
+
+    public Utf8HtmlTokenizer(IUtf8HtmlTokenSink sink, HtmlStreamingLimits limits)
+        : base(sink, limits) { }
+
+    public Utf8HtmlTokenizer(IUtf8HtmlTokenSink sink, Utf8HtmlTokenizerStateMetrics? stateMetrics)
+        : base(sink, stateMetrics) { }
+
+    public Utf8HtmlTokenizer(
+        IUtf8HtmlTokenSink sink,
+        Utf8HtmlTokenizerStateMetrics? stateMetrics,
+        HtmlStreamingLimits limits,
+        Boolean countInputBytes
+    )
+        : base(sink, stateMetrics, limits, countInputBytes) { }
+}
